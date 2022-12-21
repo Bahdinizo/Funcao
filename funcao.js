bff5b7eef3e7fb5f68a09056004fa566fecc455a
bahdinizo <t.barbara.dutradinizo@gmail.com>
2022-12-20 22:47:25 -0300

    Funções em javascript

---
+++ funcao.js
@@ -0,0 +1,36 @@
+function positivos (x, y){
+    if (x && y >=0 ){
+        alert("Positivo");
+        console.log(true);
+        
+
+    }
+    else {
+        alert("negativo");
+        console.log(false);
+    }
+}
+function ler_entrada (campo){
+    alert ("Digite o valor de x e y, para saber se são números positivos ou negativos e descubra qual é o maior valor");
+}
+function escrever_saida (x, y){
+    if (x && y >=0 ){
+        alert ("positivo");
+        console.log(true);
+        
+
+    }
+    else {
+        alert("negativo");
+        console.log(false);
+
+    }
+}
+function mostra_maior (x, y) {
+    if (x>y) {
+        alert ("x é maior que y");
+    }
+    else {
+        alert ("y é maior que x");
+    }
+}
